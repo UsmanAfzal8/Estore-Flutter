8a2d4cbcb9177dbb6342ef2f6de9ced6a527aad1
Usman Afzal <usmanafzal854@gmail.com>
2021-07-27 23:45:45 -0700

    Add all my files

--- estore/lib/Pages/registerpage.dart
+++ estore/lib/Pages/registerpage.dart
@@ -16,6 +16,78 @@ class _RegisterPageState extends State<RegisterPage> {
         centerTitle: true,
         title: Text("Register Page "),
       ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Form(
+              child: Column(
+                children: [
+                  Text(
+                    "Registration",
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "UserName",
+                          hintText: "Enter Name",
+                          icon: Icon(
+                            Icons.face,
+                          ),
+                        ),
+                      )),
+                  Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Email",
+                          hintText: "Enter Valid Email",
+                          icon: Icon(
+                            Icons.mail,
+                          ),
+                        ),
+                      )),
+                  Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                      child: TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Password",
+                          hintText: "Enter Password",
+                          icon: Icon(
+                            Icons.lock,
+                          ),
+                        ),
+                      )),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20),
+                    child: Column(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text("Signup"),
+                          style: ElevatedButton.styleFrom(
+                            primary: Theme.of(context).primaryColor,
+                            
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
